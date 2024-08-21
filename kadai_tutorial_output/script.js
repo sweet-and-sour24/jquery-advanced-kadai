93101e4a9852d64d47746dc90dda6e0f5f2d6f95
”sweet-and-sour24” <sweet.and.sour24@icloud.com>
2024-08-21 15:52:38 +0900

    4章

--- kadai_tutorial_output/script.js
+++ kadai_tutorial_output/script.js
@@ -6,11 +6,19 @@ $(function () {
       marginLeft: 20,
     }, 100);
   });
-
   $('.button-more').on('mouseout', function () {
     $(this).animate({
       opacity: 1.0,
       marginLeft: 0
     }, 100);
+  });
+
+  // カルーセル
+  $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
   });
 });
