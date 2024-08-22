4ffeb802a5066c92db27cca3fa44b14c3443cc82
”sweet-and-sour24” <sweet.and.sour24@icloud.com>
2024-08-22 09:50:28 +0900

    6章

--- kadai_tutorial_output/script.js
+++ kadai_tutorial_output/script.js
@@ -21,4 +21,38 @@ $(function () {
     autoplaySpeed: 5000,
     arrows: false,
   });
+
+  // 送信ボタンクリック時の処理
+  $('#submit').on('click', function (event) {
+    // formタグによる送信を拒否
+    event.preventDefoult();
+
+    // 入力チェックをした結果をresultに格納
+    let result = inputCheck();
+  });
+
+  // フォーカスが外れたとき(blur)にフォームの入力チェックをする
+  $('#name').blur(function () {
+    inputCheck();
+  });
+  $('#furigana').blur(function () {
+    inputCheck();
+  });
+  $('#email').blur(function () {
+    inputCheck();
+  });
+  $('#tel').blur(function () {
+    inputCheck();
+  });
+  $('#message').blur(function () {
+    inputCheck();
+  });
+  $('#agree').click(function () {
+    inputCheck();
+  });
+
+  // お問い合わせフォームの入力チェック
+  function inputCheck() {
+    console.log('inputCheck関数の呼び出し');
+  }
 });
